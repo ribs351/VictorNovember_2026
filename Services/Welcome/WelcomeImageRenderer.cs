4b3054645aa807ce321597526b20c51dcac84d4b
Ribs <ribs351@gmail.com>
2026-02-12 10:25:21 +0700

    It is what it is

--- Services/Welcome/WelcomeImageRenderer.cs
+++ Services/Welcome/WelcomeImageRenderer.cs
@@ -66,7 +66,7 @@ public sealed class WelcomeImageRenderer
         // Draw text
         DrawTextOnBanner(
             background,
-            $"{member.Username} joined the server",
+            $"{member.DisplayName} joined the server",
             $"Member #{member.Guild.MemberCount}"
         );
 
